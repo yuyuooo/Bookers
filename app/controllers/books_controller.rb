f63a20837b3f99492b0be758a13cdbd761a27d48
yuyuooo <103011542+yuyuooo@users.noreply.github.com>
2022-04-23 01:49:55 +0000

    [Finish]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,48 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+
+  def create
+    @books = Book.all
+    @book = Book.new(book_params)
+     if @book.save
+      redirect_to book_path(@book.id), notice: "Book was successfully created."
+     else
+      render ("books/index")
+     end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id), notice: "Book was successfully updated."
+    else
+    render ("books/edit")
+    end
+  end
+
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books', notice: "Book was successfully destroyed."
+  end
+
+  private
+   def book_params
+    params.require(:book).permit(:title, :body)
+   end
+end
